d0c7548066ab2148bc586ef36220c22c3ccc1806
김도연 <piakim0119@gmail.com>
2024-11-14 18:42:48 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 12.12 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
@@ -15,4 +15,9 @@ class Solution {
         
         return answer;
     }
+    
+    public static void main (String args[]) {        
+        Solution sol = new Solution();
+        System.out.println(sol.solution(3, 5));
+    }
 }
