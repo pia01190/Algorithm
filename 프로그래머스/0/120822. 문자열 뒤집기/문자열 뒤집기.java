b9535e76548e4396b617eb2490120970e3b2fcd3
pia01190 <piakim0119@gmail.com>
2025-02-14 10:48:55 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.05 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,6 +1,6 @@
 class Solution {
     public String solution(String my_string) {
-        StringBuffer str = new StringBuffer(my_string);
+        StringBuilder str = new StringBuilder(my_string);
         String answer = str.reverse().toString();
         
         return answer;
