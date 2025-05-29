0e4d9e58d9ef15cfec0bb54435cf96aa592f022b
pia01190 <piakim0119@gmail.com>
2025-05-29 10:29:59 +0900

    [level 0] Title: 팩토리얼, Time: 0.02 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120848. 팩토리얼/팩토리얼.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int num = 1;
+        
+        for (int i = 1; i <= 10; i++) {
+            num *= i;
+            if (num <= n) {
+                answer = i;
+            }
+        }
+        
+        return answer;
+    }
+}
