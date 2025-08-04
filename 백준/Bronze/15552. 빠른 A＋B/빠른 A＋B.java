d28e49075eca434a0909e55fb571cd9cf94d21bc
pia01190 <piakim0119@gmail.com>
2025-08-05 00:11:40 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 888 ms, Memory: 288600 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int T = Integer.parseInt(br.readLine());
+        
+        for (int i = 0; i < T; i++) {
+            String[] nums = br.readLine().split(" ");
+            int A = Integer.parseInt(nums[0]);
+            int B = Integer.parseInt(nums[1]);
+            bw.write((A + B) + "\n");
+        }
+        
+        bw.flush();
+        bw.close();
+    }
+}
