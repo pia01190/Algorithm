926953d154825e7f4a2ea3229360e5830b778845
pia01190 <piakim0119@gmail.com>
2025-09-03 22:54:19 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 87.71 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,31 +2,26 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
         Map<String, Integer> map = new HashMap<>();
         
         for (int i = 0; i < completion.length; i++) {
             String name = completion[i];
-            
-            if (map.containsKey(name)) {
-                map.put(name, map.get(name) + 1);
-            } else {
-                map.put(name, 1);
-            }
+            map.put(name, map.getOrDefault(name, 0) + 1);
         }
         
         for (int i = 0; i < participant.length; i++) {
             String name = participant[i];
+            int count = map.getOrDefault(name, 0);
             
-            if (!map.containsKey(name)) {
-                answer = name;
-            } else if (map.containsKey(name) && map.get(name) == 1) {
+            if (count == 0) {
+                return name;
+            } else if (count == 1) {
                 map.remove(name);
-            } else if (map.containsKey(name) && map.get(name) > 1) {
-                map.put(name, map.get(name) - 1);
+            } else if (count > 1) {
+                map.put(name, count - 1);
             }
         }
         
-        return answer;
+        return "";
     }
 }
