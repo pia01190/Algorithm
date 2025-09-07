0bb35b4122b1b46d9fc7ff8f38788c3f30783ef9
pia01190 <piakim0119@gmail.com>
2025-09-07 20:10:40 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.05 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int solution(int n, int w, int num) {
+        int height = (n + w - 1) / w;
+        int[][] box = new int[height][w];
+        int boxNum = 1;
+        int row = 0;
+        int col = 0;
+        
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < w; j++) {
+                if(boxNum > n) {
+                    break;
+                }
+                
+                if (i % 2 == 0) {
+                    box[i][j] = boxNum++;
+                } else {
+                    box[i][w - 1 - j] = boxNum++;
+                }
+            }
+        }
+        
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < w; j++) {
+                if (box[i][j] == num) {
+                    row = i;
+                    col = j;
+                    break;
+                }
+            }
+        }
+        
+        int count = 0;
+        for(int i = row; i < height; i++) {
+            if(box[i][col] != 0) {
+                count++;
+            }
+        }
+        
+        return count;
+    }
+}
