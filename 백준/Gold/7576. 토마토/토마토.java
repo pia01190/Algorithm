7ae03d5dbf73317339fbdfe149a2eb47db9bd877
pia01190 <piakim0119@gmail.com>
2025-09-11 11:53:57 +0900

    [Gold V] Title: 토마토, Time: 600 ms, Memory: 128276 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -8,14 +8,16 @@ class Main {
 
         int m = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(st.nextToken());
-        int[][] box = new int[n][m];
 
+        int[][] box = new int[n][m];
         Queue<int[]> queue = new LinkedList<>();
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
+
             for (int j = 0; j < m; j++) {
                 box[i][j] = Integer.parseInt(st.nextToken());
+
                 if (box[i][j] == 1) {
                     queue.add(new int[] {i, j, 0});
                 }
@@ -28,7 +30,9 @@ class Main {
     private static int bfs(int[][] box, Queue<int[]> queue) {
         int rowLength = box.length;
         int colLength = box[0].length;
+
         int day = 0;
+        int totalDay = 0;
 
         int[] dx = {1, 0, -1, 0};
         int[] dy = {0, 1, 0, -1};
@@ -40,41 +44,40 @@ class Main {
             day = temp[2];
 
             for (int k = 0; k < dx.length; k++) {
-                int nextRow = row + dx[k];
-                int nextCol = col + dy[k];
+                int newRow = row + dx[k];
+                int newCol = col + dy[k];
 
-                if (!isMapIn(nextRow, nextCol, rowLength, colLength)) {
+                if (!isMapIn(newRow, newCol, rowLength, colLength)) {
                     continue;
                 }
 
-                if (box[nextRow][nextCol] == 0) {
-                    queue.add(new int[]{nextRow, nextCol, day + 1});
-                    box[nextRow][nextCol] = 1;
+                if (box[newRow][newCol] == 0) {
+                    queue.add(new int[] {newRow, newCol, day + 1});
+                    box[newRow][newCol] = 1;
                 }
             }
         }
 
-        if (checkTomato(box, rowLength, colLength)) {
+        if (!checkedTomato(box, rowLength, colLength)) {
             return -1;
         }
 
-        int maxDay = 0;
-        return Math.max(maxDay, day);
+        return Math.max(totalDay, day);
     }
 
-    private static boolean isMapIn(int row, int col, int rowLength, int colLength) {
-        return row >= 0 && row < rowLength && col >= 0 && col < colLength;
+    private static boolean isMapIn(int newRow, int newCol, int rowLength, int colLength) {
+        return newRow < rowLength && newRow >= 0 && newCol < colLength && newCol >= 0;
     }
 
-    private static boolean checkTomato(int[][] box, int rowLength, int colLength) {
+    private static boolean checkedTomato(int[][] box, int rowLength, int colLength) {
         for (int i = 0; i < rowLength; i++) {
             for (int j = 0; j < colLength; j++) {
-                if(box[i][j] == 0) {
-                    return true;
+                if (box[i][j] == 0) {
+                    return false;
                 }
             }
         }
 
-        return false;
+        return true;
     }
 }
