a5f42845481fe7e730df7a5035d82a53385f7f8d
pia01190 <piakim0119@gmail.com>
2025-09-12 18:36:36 +0900

    [level 2] Title: 카펫, Time: 0.11 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int row = 0;
+        int col = 0;
+        int carpet = brown + yellow;
+        
+        for (int i = 1; i <= carpet; i++) {
+            row = i;
+            col = carpet / row;
+            
+            if (row < 3) {
+                continue;
+            }
+            
+            if ((row - 2) * (col - 2) == yellow && row >= col && row * col == carpet) {
+                break;
+            }
+        }
+        
+        return new int[] {row, col};
+    }
+}
