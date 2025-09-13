42054f0688c8892e17b8dba8380dc0cf887a81d0
pia01190 <piakim0119@gmail.com>
2025-09-13 15:51:56 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -4,23 +4,9 @@ class Solution
     {
         int answer = 0;
         
-        while (true) {
-            if (a == b) {
-                break;
-            }
-            
-            if (a % 2 == 1) {
-                a = (a + 1) / 2;
-            } else {
-                a = a / 2;
-            }
-            
-            if (b % 2 == 1) {
-                b = (b + 1) / 2;
-            } else {
-                b = b / 2;
-            }
-            
+        while (a != b) {
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
             answer++;
         }
 
