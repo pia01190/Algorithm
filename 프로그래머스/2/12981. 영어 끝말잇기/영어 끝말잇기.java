d1e23bec65185d18a9e27a0dac04ce9c0d6635a3
pia01190 <piakim0119@gmail.com>
2025-09-16 13:48:51 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.08 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -2,43 +2,20 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int n, String[] words) {
-        Queue<String> wordQueue = new LinkedList<>();
-        wordQueue.add(words[0]);
+        Set<String> wordSet = new HashSet<>();
+        wordSet.add(words[0]);
         char last = words[0].charAt(words[0].length() - 1);
-        
-        int num = 1; // 가장 먼저 탈락하는 사람의 번호
-        int turn = 1; // 그 사람이 자신의 몇 번째 차례에 탈락하는지
-        boolean fail = false;
         
         for (int i = 1; i < words.length; i++) {
             char first = words[i].charAt(0);
             
-            if (first != last) {
-                num++;
-                fail = true;
-                break;
+            if (first != last || !wordSet.add(words[i])) {
+                return new int[] {i % n + 1, i / n + 1};
             }
             
-            if (wordQueue.contains(words[i])) {
-                num++;
-                fail = true;
-                break;
-            }
-            
-            wordQueue.add(words[i]);
-            num++;
             last = words[i].charAt(words[i].length() - 1);
-            
-            if (num == n) {
-                num = 0;
-                turn++;
-            }
-        }
-        
-        if (fail == false) {
-            return new int[] {0, 0};
         }
 
-        return new int[] {num, turn};
+        return new int[] {0, 0};
     }
 }
