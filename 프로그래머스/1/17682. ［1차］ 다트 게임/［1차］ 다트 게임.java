3b0d2948fedbd3917fa0dc3abafc1d8107e14d2e
pia01190 <piakim0119@gmail.com>
2025-09-22 11:19:34 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.07 ms, Memory: 94.8 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.java
@@ -7,25 +7,11 @@ class Solution {
         for (int i = 0; i < dartResult.length(); i++) {
             char score = dartResult.charAt(i);
             
-            if (score == 'S') {
-                result[++index] = num;
-            } else if (score == 'D') {
-                num *= num;
-                result[++index] = num;
-            } else if (score == 'T') {
-                num = num * num * num;
-                result[++index] = num;
-            } else if (score == '*') {
-                result[index] *= 2;
-                
-                if (index > 0) {
-                    result[index - 1] *= 2;
-                }
-            } else if (score == '#') {
-                result[index] *= -1;
-            } else if (score == '0') {
-                num = (num == 1) ? 10 : 0;
-            } else if (dartResult.charAt(i) == '0' && dartResult.charAt(i - 1) == '1' && i > 0) {
+            if (score == 'S' || score == 'D' || score == 'T') {
+                index = area(result, index, num, score);
+            } else if (score == '*' || score == '#') {
+                award(result, index, score);
+            } else if (i > 0 && dartResult.charAt(i) == '0' && dartResult.charAt(i - 1) == '1') {
                 num = 10;
             } else {
                 num = Integer.parseInt(String.valueOf(score));
@@ -38,5 +24,31 @@ class Solution {
         }
         
         return answer;
+    }
+    
+    private static int area(int[] result, int index, int num, char score) {
+        if (score == 'S') {
+            result[++index] = num;
+        } else if (score == 'D') {
+            num *= num;
+            result[++index] = num;
+        } else if (score == 'T') {
+            num = num * num * num;
+            result[++index] = num;
+        }
+        
+        return index;
+    }
+    
+    private static void award(int[] result, int index, char score) {
+        if (score == '*') {
+            result[index] *= 2;
+                
+            if (index > 0) {
+                result[index - 1] *= 2;
+            }
+        } else if (score == '#') {
+            result[index] *= -1;
+        }
     }
 }
