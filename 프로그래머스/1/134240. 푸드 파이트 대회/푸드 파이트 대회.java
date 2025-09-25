835c53e06cf8219ebc1a916e292c1173767ebb49
pia01190 <piakim0119@gmail.com>
2025-09-25 18:29:57 +0900

    Update 푸드 파이트 대회.java

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,7 +1,6 @@
 class Solution {
     public String solution(int[] food) {
         StringBuilder sb = new StringBuilder();
-        StringBuilder answer = new StringBuilder();
         
         for (int i = 1; i < food.length; i++) {
             for (int j = 0; j < food[i] / 2; j++) {
@@ -9,9 +8,6 @@ class Solution {
             }
         }
         
-        answer.append(sb).append("0");
-        answer.append(sb.reverse());
-        
-        return answer.toString();
+        return sb.toString() + "0" + sb.reverse();
     }
 }
