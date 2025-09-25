9d077d61ff874354ec7abb68e445e5a2c54590d9
pia01190 <piakim0119@gmail.com>
2025-09-25 17:03:31 +0900

    Update 기능개발.java

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,30 +2,37 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> result = new ArrayList<>();
-        int totalDay = ((100 - progresses[0]) % speeds[0] == 0) ? (100 - progresses[0]) / speeds[0] : (100 - progresses[0]) / speeds[0] + 1;
+        List<Integer> days = new ArrayList<>();
+        int totalDay = calculateDays(progresses[0], speeds[0]);
         int count = 1;
         
         for (int i = 1; i < progresses.length; i++) {
-            int day = ((100 - progresses[i]) % speeds[i] == 0) ? (100 - progresses[i]) / speeds[i] : (100 - progresses[i]) / speeds[i] + 1;
+            int day = calculateDays(progresses[i], speeds[i]);
+            
             if (day > totalDay) {
+                days.add(count);
                 totalDay = day;
-                result.add(count);
                 count = 1;
             } else {
                 count++;
             }
             
             if (i == progresses.length - 1) {
-                result.add(count);
+                days.add(count);
             }
         }
         
-        int[] answer = new int[result.size()];
-        for (int i = 0; i < result.size(); i++) {
-            answer[i] = result.get(i);
+        int[] result = new int[days.size()];
+        for (int i = 0; i < days.size(); i++) {
+            result[i] = days.get(i);
         }
         
-        return answer;
+        return result;
+    }
+    
+    private int calculateDays(int progresses, int speeds) {
+        return ((100 - progresses) % speeds == 0) ?
+            (100 - progresses) / speeds :
+            (100 - progresses) / speeds + 1;
     }
 }
