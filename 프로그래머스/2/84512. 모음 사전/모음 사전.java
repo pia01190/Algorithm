61c9b6e0189b7e759751cf7a4413eff366f93428
pia01190 <piakim0119@gmail.com>
2025-09-30 12:18:41 +0900

    [level 2] Title: 모음 사전, Time: 7.46 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,31 +1,30 @@
 class Solution {
     
-    String alphabet = "AEIOU";
-    StringBuilder sb = new StringBuilder();
-    int count = 0;
-    int answer = 0;
+    private String alphabet = "AEIOU";
+    private StringBuilder sb = new StringBuilder();
+    private int count;
+    private int answer;
     
     public int solution(String word) {
+        count = 0;
+        answer = 0;
         dfs(sb, word);
         return answer;
     }
     
     private void dfs(StringBuilder sb, String word) {
-        if (sb.length() > 5) {
+        if (sb.toString().equals(word)) {
+            answer = count;
             return;
         }
         
-        if (sb.length() > 0) {
-            count++;
-            
-            if (sb.toString().equals(word)) {
-                answer = count;
-                return;
-            }
+        if (sb.length() >= 5) {
+            return;
         }
         
         for (int i = 0; i < alphabet.length(); i++) {
             sb.append(alphabet.charAt(i));
+            count++;
             dfs(sb, word);
             sb.deleteCharAt(sb.length() - 1);
         }
